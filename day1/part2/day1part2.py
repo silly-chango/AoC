43151b27340197437992eebea918a5050b38adcf
Dany <dra0171@protonmail.com>
2021-12-08 08:05:03 -0500

    final solution of part2

--- day1/part2/day1part2.py
+++ day1/part2/day1part2.py
@@ -1,25 +1,22 @@
 import sys
+
+
 def open_file(txt):
-    file_object= open(txt,"r")
-    c = 0
+    queue = []
+    c=0
     count = 0
-    tree = []
-
-    for i in file_object:
-        print(i)
-
-"""
-    for i in file_object:
-        tree.append(int(i))
-        if c < int(i):
-            count+=1
-        c = int(i)
-    return count - 1
-    # we subtract 1 due to the first value not counting as it is 
-    # the starting valuee
-"""
+    with open(txt) as file:
+        lines = file.readlines()
+        for i in range(len(lines)-2):
+            queue.extend((int(lines[i]),int(lines[i+1]),int(lines[i+2])))
+            if c < sum(queue):
+                count +=1
+            c = sum(queue)
+            queue.clear()
+    return count -1
 def main():
     print(open_file(sys.argv[1]))
+
 
 if __name__ == '__main__':
     main()
